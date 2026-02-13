d3db1fa2139ec5c6fc292fea7a1e0f573d257e0f
Kien <kientrungnguyen001@gmail.com>
2026-02-13 22:11:45 +0700

    Feature: API check token when user refresh this website

--- src/main/java/com/SocialPlat/SocialPlat/security/filter/JwtFilter.java
+++ src/main/java/com/SocialPlat/SocialPlat/security/filter/JwtFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -14,40 +15,30 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @Component
 public class JwtFilter extends OncePerRequestFilter {
     private  final JWTutil jwtUtil;
     private final CustomerUserDetailService userDetailService;
-    public JwtFilter(JWTutil jwtUtil, CustomerUserDetailService userDetailService) {
-        this.jwtUtil = jwtUtil;
-        this.userDetailService = userDetailService;
-    }
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
         String header = request.getHeader("Authorization");
-
-        // 1. Nếu không có header hoặc không bắt đầu bằng Bearer, cho đi tiếp luôn (để permitAll xử lý)
         if (header == null || !header.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
-
         try {
             String token = header.substring(7);
             String username = jwtUtil.extractUsername(token);
 
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 UserDetailimp user = (UserDetailimp) userDetailService.loadUserByUsername(username);
-
                 UsernamePasswordAuthenticationToken auth =
                         new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
         } catch (Exception e) {
-            // Nếu token sai/hết hạn, có thể log lỗi ở đây
             System.out.println("JWT Error: " + e.getMessage());
         }
 
